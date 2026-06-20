eb36ca53f16d2ad0e6d70e2d82d55ac61f08f095
jeongjaem <1105woals@soongsil.ac.kr>
2026-06-20 09:09:28 +0000

    ㅁㄴㅇㄹ

--- main.py
+++ main.py
@@ -5,51 +5,37 @@ from candles import load_initial_candles
 from websocket_client import start
 from strategy import analyze
 from price_manager import get_price
-from state import watch_symbols
-from state import last_alert
+from state import watch_symbols, last_alert
 from config import ALERT_COOLDOWN
 
 
 def refresh_watchlist():
-
     watch_symbols.clear()
-
     coins = get_top_futures()
 
     print("\n====== TOP30 ======\n")
 
     for i, coin in enumerate(coins, 1):
-
         symbol = coin["symbol"]
-
         watch_symbols.append(symbol)
 
-        print(
-            f"{i:2d}. "
-            f"{symbol:<20}"
-            f"{coin['gain']:.2f}%"
-        )
+        print(f"{i:2d}. {symbol:<20}{coin['gain']:.2f}%")
 
     print("\n공식 5분봉 다운로드...\n")
 
     success = 0
 
     for symbol in watch_symbols:
-
         if load_initial_candles(symbol):
             success += 1
 
     print(f"{success}/{len(watch_symbols)} 완료")
-
     print("\nWebSocket 연결중...\n")
 
 
 def monitor():
-
     while True:
-
         for symbol in watch_symbols:
-
             price = get_price(symbol)
 
             if price is None:
@@ -61,32 +47,28 @@ def monitor():
                 continue
 
             if result["near_ma"]:
+                now = time.time()
+
+                if symbol in last_alert:
+                    if now - last_alert[symbol] < ALERT_COOLDOWN:
+                        continue
+
+                last_alert[symbol] = now
 
                 print("=" * 70)
-
                 print(f"🚨 {symbol}")
-
                 print(f"현재가 : {price}")
-
                 print(f"MA60 : {result['ma60']:.6f}")
-
                 print(f"거리 : {result['distance']:.3f}%")
-
                 print(f"고점 : {result['high']}")
-
                 print(f"저점 : {result['low']}")
-
                 print(f"하락률 : {result['drop']:.2f}%")
 
         time.sleep(15)
 
 
 if __name__ == "__main__":
-
     refresh_watchlist()
-
     start()
-
     time.sleep(3)
-
     monitor()
